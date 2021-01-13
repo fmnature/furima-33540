2eb3a612e80f598685504bd196dcaa740320d67f
fmnature <sekaowa75.uhh@gmail.com>
2021-01-13 20:20:17 +0900

    商品購入機能修正１

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,24 +7,17 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
-
   def create
     @order_form = OrderForm.new(order_params)
-    item = Item.find(params[:item_id])
-    if current_user.id == item.user_id || item.order.present?
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
     if @order_form.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      card_info
       @order_form.save
       redirect_to root_path
     else
-      @item = item
       render action: :index
     end
   end
@@ -32,5 +25,14 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_form).permit(:post_num, :area_id, :municipal, :address, :build_name, :phone_num, :order_id).merge(token: params[:token], buyer_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def card_info
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
